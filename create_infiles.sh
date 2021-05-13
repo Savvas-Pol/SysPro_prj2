b61d17af3dded469f2be67542b7d8d1a3e71a0a6
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-05-14 02:31:27 +0300

    Bash script

--- create_infiles.sh
+++ create_infiles.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+ARGC=$#
+MAX_ARGS=3
+
+if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
+	if [ $3 -gt 0 ]; then	#check values of parameters
+		mkdir $2;			#create directory
+
+	else
+		echo "Wrong values!!!\n";
+	fi
+else
+	echo "Wrong arguments!!!\n";
+fi
