6fe4d335eaffc8b29171d5199606d48af5dadf25
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-05-17 23:17:19 +0300

    Bash script check arguments

--- create_infiles.sh
+++ create_infiles.sh
@@ -2,14 +2,27 @@
 
 ARGC=$#
 MAX_ARGS=3
+counter=0
+
+declare -a countries
 
 if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
-	if [ $3 -gt 0 ]; then	#check values of parameters
-		mkdir $2;			#create directory
+	if [[ -f $1 ]]; then		#check if first argument is a file
+		if [ $3 -gt 0 ]; then	#check values of parameters
+			mkdir $2;			#create directory
 
+			# while IFS= read -r line; do 			#read inputFile line by line
+			# 	countries[$counter] = ( $(awk -F ' ') );
+			# 	echo "countries[$counter]";
+			# 	let counter=counter+1;
+			# done < "$1"
+
+		else
+			echo "Wrong values!!!";
+		fi
 	else
-		echo "Wrong values!!!\n";
+		echo "$1 is not a file!!!";
 	fi
 else
-	echo "Wrong arguments!!!\n";
+	echo "Wrong arguments!!!";
 fi
